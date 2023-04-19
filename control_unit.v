6bf49e32a76c3b31e49313ca78fb3a3b6452a29e
moneerzaki <78418503+moneerzaki@users.noreply.github.com>
2023-04-19 20:17:39 +0200

    control unit
    needs more editing..... 
    + edits for Miro 
    + alusrc2 instead auipc .....

--- control_unit.v
+++ control_unit.v
@@ -1,5 +1,26 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2023 10:21:23 PM
+// Design Name: 
+// Module Name: CONTROL UNIT
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 //`include "defines.v"
-/*`define     OPCODE_Branch   5'b11_000
+/*
+`define     OPCODE_Branch   5'b11_000
 `define     OPCODE_Load     5'b00_000
 `define     OPCODE_Store    5'b01_000
 `define     OPCODE_JALR     5'b11_001
@@ -7,49 +28,65 @@
 `define     OPCODE_Arith_I  5'b00_100
 `define     OPCODE_Arith_R  5'b01_100
 `define     OPCODE_AUIPC    5'b00_101
-`define     OPCODE_LUI      5'b01_101*/
-module control_unit(input[31:0] instruction, output reg branch, memread, output reg[1:0]memtoreg, output reg memwrite, alusrc, regwrite, AUIPC, ConcEn, Shift, JALR, JAL, output reg[3:0] aluop);
+`define     OPCODE_LUI      5'b01_101
+*/
+
+module ControlUnit
+(
+    input[31:0] Istruction, 
+    output reg Branch, 
+    output reg MemRead, 
+    output reg[1:0]MemtoReg, 
+    output reg MemWrite, 
+    output reg ALUSrc, 
+    output reg RegWrite, 
+    output reg AUIPC, 
+    output reg ConcEn, 
+    output reg Shift, 
+    output reg JALR,
+    output reg JAL,
+    output reg [3:0] ALUOp);
 
     always @(instruction) begin
         case(instruction[6:2])
-            5'b01_100: begin
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b00; aluop = 2'b10; memwrite = 1'b0; alusrc = 1'b0; regwrite = 1'b1; 
+            `OPCODE_Arith_R: begin
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b00; ALUOp = 2'b10; MemWrite = 1'b0; ALUSrc = 1'b0; RegWrite = 1'b1; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b0; JAL = 1'b0; 
             end
-            5'b00_000: begin
-                branch = 1'b0; memread = 1'b1; memtoreg = 2'b01; aluop = 2'b00; memwrite = 1'b0; alusrc = 1'b1; regwrite = 1'b1; 
+            `OPCODE_Load: begin
+                Branch = 1'b0; MemRead = 1'b1; MemtoReg = 2'b01; ALUOp = 2'b00; MemWrite = 1'b0; ALUSrc = 1'b1; RegWrite = 1'b1; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b0; JAL = 1'b0;
             end
-            5'b01_000: begin
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b00; aluop = 2'b00; memwrite = 1'b1; alusrc = 1'b1; regwrite = 1'b0; 
+            `OPCODE_Store: begin
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b00; ALUOp = 2'b00; MemWrite = 1'b1; ALUSrc = 1'b1; RegWrite = 1'b0; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b0; JAL = 1'b0;
             end                                           
-            5'b11_000: begin                               
-                branch = 1'b1; memread = 1'b0; memtoreg = 2'b00; aluop = 2'b01; memwrite = 1'b0; alusrc = 1'b0; regwrite = 1'b0; 
+            `OPCODE_Branch: begin                               
+                Branch = 1'b1; MemRead = 1'b0; MemtoReg = 2'b00; ALUOp = 2'b01; MemWrite = 1'b0; ALUSrc = 1'b0; RegWrite = 1'b0; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b1; JALR =1'b0; JAL = 1'b0;
             end                                           
-            5'b00_101: begin                               
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b00; aluop = 2'b00; memwrite = 1'b0; alusrc = 1'b1; regwrite = 1'b1; 
+            `OPCODE_AUIPC: begin                               
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b00; ALUOp = 2'b00; MemWrite = 1'b0; ALUSrc = 1'b1; RegWrite = 1'b1; 
                 AUIPC = 1'b1; ConcEn = 1'b1; Shift = 1'b0; JALR =1'b0; JAL = 1'b0;
             end                                           
-            5'b00_100: begin                               
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b00; aluop = 2'b10; memwrite = 1'b0; alusrc = 1'b1; regwrite = 1'b1; 
+            `OPCODE_Arith_I: begin                               
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b00; ALUOp = 2'b10; MemWrite = 1'b0; ALUSrc = 1'b1; RegWrite = 1'b1; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b0; JAL = 1'b0;
             end                                           
-            5'b11_011: begin                               
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b10; aluop = 2'b00; memwrite = 1'b0; alusrc = 1'b1; regwrite = 1'b1; 
+            `OPCODE_JAL: begin                               
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b10; ALUOp = 2'b00; MemWrite = 1'b0; ALUSrc = 1'b1; RegWrite = 1'b1; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b0; JAL = 1'b1;
             end                                           
-            5'b11_001: begin                               
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b10; aluop = 2'b00; memwrite = 1'b0; alusrc = 1'b1; regwrite = 1'b1; 
+            `OPCODE_JALR: begin                               
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b10; ALUOp = 2'b00; MemWrite = 1'b0; ALUSrc = 1'b1; RegWrite = 1'b1; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b1; JAL = 1'b0;
             end                                           
-            5'b01_101: begin                               
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b11; aluop = 2'b11; memwrite = 1'b0; alusrc = 1'b1; regwrite = 1'b1; 
+            `OPCODE_LUI: begin                               
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b11; ALUOp = 2'b11; MemWrite = 1'b0; ALUSrc = 1'b1; RegWrite = 1'b1; 
                 AUIPC = 1'b0; ConcEn = 1'b1; Shift = 1'b0; JALR =1'b0; JAL = 1'b0;
             end                                           
             default: begin                                
-                branch = 1'b0; memread = 1'b0; memtoreg = 2'b00; aluop = 2'b10; memwrite = 1'b0; alusrc = 1'b0; regwrite = 1'b0; 
+                Branch = 1'b0; MemRead = 1'b0; MemtoReg = 2'b00; ALUOp = 2'b10; MemWrite = 1'b0; ALUSrc = 1'b0; RegWrite = 1'b0; 
                 AUIPC = 1'b0; ConcEn = 1'b0; Shift = 1'b0; JALR =1'b0; JAL = 1'b0;
             end
         endcase
